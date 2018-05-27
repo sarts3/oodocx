a780272ff2d8cd5dd217f9da3330534df99eb3d2
sarts3 <carlossarti3@gmail.com>
2018-05-27 12:20:32 +0200

    changing import method2

--- oodocx/oodocx.py
+++ oodocx/oodocx.py
@@ -18,8 +18,12 @@ import collections
 import stat
 import tempfile
 from lxml import etree
-from helper_functions import remove_readonly, add_relationship, get_image_size
-from write_files import write_rels, setup_comments, write_content_types
+
+
+import struct
+import imghdr
+import stat
+import os
 
 log = logging.getLogger(__name__)
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'template')
@@ -57,6 +61,7 @@ NSPREFIXES = {
     'dcterms':  'http://purl.org/dc/terms/',
     # Special xml namespace
     'xml': 'http://www.w3.org/XML/1998/namespace'}
+
 
 COLOR_MAP = {
     'black': '000000',
@@ -1362,3 +1367,217 @@ def remove_formatting(element):
         if (descendant.tag == '{' + NSPREFIXES['w'] + '}pPr' or
         descendant.tag == '{' + NSPREFIXES['w'] + '}rPr'):
             descendant.getparent().remove(descendant)
+
+
+def get_image_size(fname):
+	'''Determine the image type of fhandle and return its size.
+	from draco'''
+	fhandle = open(fname, 'rb')
+	head = fhandle.read(24)
+	if len(head) != 24:
+		return
+	if imghdr.what(fname) == 'png':
+		check = struct.unpack('>i', head[4:8])[0]
+		if check != 0x0d0a1a0a:
+			return
+		width, height = struct.unpack('>ii', head[16:24])
+	elif imghdr.what(fname) == 'gif':
+		width, height = struct.unpack('<HH', head[6:10])
+	elif imghdr.what(fname) == 'jpeg':
+		try:
+			fhandle.seek(0) # Read 0xff next
+			size = 2
+			ftype = 0
+			while not 0xc0 <= ftype <= 0xcf:
+				fhandle.seek(size, 1)
+				byte = fhandle.read(1)
+				while ord(byte) == 0xff:
+					byte = fhandle.read(1)
+				ftype = ord(byte)
+				size = struct.unpack('>H', fhandle.read(2))[0] - 2
+			# We are at a SOFn block
+			fhandle.seek(1, 1)  # Skip `precision' byte.
+			height, width = struct.unpack('>HH', fhandle.read(4))
+		except Exception: #IGNORE:W0703
+			return
+	else:
+		return
+	return width, height
+
+def remove_readonly(fn, path, excinfo):
+    if fn is os.rmdir:
+        os.chmod(path, stat.S_IWRITE)
+        os.rmdir(path)
+    elif fn is os.remove:
+        os.chmod(path, stat.S_IWRITE)
+        os.remove(path)
+
+def add_relationship(document, target, type):
+    '''checks Relationships element to see if element is included,
+    adds it if not, returns element's rId or None'''
+    relationship_items = [child.items() for child in document.relationships.getchildren()]
+    flat_relationships = sum(relationship_items, [])
+    id_numbers = sorted([int(item[1][3:]) for item in flat_relationships if item[0] == 'Id'])
+    rId_number = len(id_numbers) + 1
+    for count, number in enumerate(id_numbers, start=1):
+        if count != number:
+            rId_number = count + 1
+            break
+    if target not in [child[1] for child in flat_relationships] or 'media' in target:
+        document.relationships.append(makeelement('Relationship', nsprefix=None,
+        attributes={'Id': 'rId' + str(rId_number),
+                    'Target': target,
+                    'Type': type}))
+        return 'rId' + str(rId_number)
+    else:
+        return None
+
+def makeelement(tagname, tagtext=None, nsprefix='w', attributes=None, attrnsprefix=None):
+    '''Create an element & return it'''
+    # Deal with list of nsprefix by making namespacemap
+    namespacemap = None
+    if isinstance(nsprefix, list):
+        namespacemap = {}
+        for prefix in nsprefix:
+            namespacemap[prefix] = NSPREFIXES[prefix]
+        # FIXME: rest of code below expects a single prefix
+        nsprefix = nsprefix[0]
+    if nsprefix:
+        namespace = '{' + NSPREFIXES[nsprefix] + '}'
+    else:
+        # For when namespace = None
+        namespace = ''
+    newelement = etree.Element(namespace+tagname, nsmap=namespacemap)
+    # Add attributes with namespaces
+    if attributes:
+        # If they haven't bothered setting attribute namespace, use an empty string
+        # (equivalent of no namespace)
+        if not attrnsprefix:
+           # Quick hack: it seems every element that has a 'w' nsprefix for its tag uses the same prefix for its attributes
+            if nsprefix == 'w':
+                attributenamespace = namespace
+            else:
+                attributenamespace = ''
+        else:
+            attributenamespace = '{'+NSPREFIXES[attrnsprefix]+'}'
+
+        for tagattribute in attributes:
+            newelement.set(attributenamespace+tagattribute, attributes[tagattribute])
+    if tagtext:
+        newelement.text = tagtext
+    newelement.prefix
+    return newelement
+
+def makeelement(tagname, tagtext=None, nsprefix='w', attributes=None, attrnsprefix=None):
+    '''Create an element & return it'''
+    # Deal with list of nsprefix by making namespacemap
+    namespacemap = None
+    if isinstance(nsprefix, list):
+        namespacemap = {}
+        for prefix in nsprefix:
+            namespacemap[prefix] = NSPREFIXES[prefix]
+        # FIXME: rest of code below expects a single prefix
+        nsprefix = nsprefix[0]
+    if nsprefix:
+        namespace = '{' + NSPREFIXES[nsprefix] + '}'
+    else:
+        # For when namespace = None
+        namespace = ''
+    newelement = etree.Element(namespace+tagname, nsmap=namespacemap)
+    # Add attributes with namespaces
+    if attributes:
+        # If they haven't bothered setting attribute namespace, use an empty string
+        # (equivalent of no namespace)
+        if not attrnsprefix:
+           # Quick hack: it seems every element that has a 'w' nsprefix for its tag uses the same prefix for its attributes
+            if nsprefix == 'w':
+                attributenamespace = namespace
+            else:
+                attributenamespace = ''
+        else:
+            attributenamespace = '{'+NSPREFIXES[attrnsprefix]+'}'
+
+        for tagattribute in attributes:
+            newelement.set(attributenamespace+tagattribute, attributes[tagattribute])
+    if tagtext:
+        newelement.text = tagtext
+    newelement.prefix
+    return newelement
+
+def write_rels():
+    relationships = etree.fromstring(
+            '<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"></Relationships>')
+    relationship3 = etree.fromstring('<Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties"'
+    ' Target="docProps/app.xml"/>')
+    relationship2 = etree.fromstring('<Relationship Id="rId2" '
+    'Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties"'
+    ' Target="docProps/core.xml"/>')
+    relationship1 = etree.fromstring('<Relationship Id="rId1" '
+    'Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument"'
+    ' Target="word/document.xml"/>')
+    relationships.append(relationship3)
+    relationships.append(relationship2)
+    relationships.append(relationship1)
+    return relationships
+
+def write_content_types():
+    content_types = etree.fromstring(
+    '<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"> '
+    '<Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/> '
+    '<Default Extension="xml" ContentType="application/xml"/> '
+    '<Override PartName="/word/document.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"/> '
+    '<Override PartName="/word/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.styles+xml"/> '
+    '<Override PartName="/word/stylesWithEffects.xml" ContentType="application/vnd.ms-word.stylesWithEffects+xml"/> '
+    '<Override PartName="/word/settings.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.settings+xml"/> '
+    '<Override PartName="/word/webSettings.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.webSettings+xml"/> '
+    '<Override PartName="/word/fontTable.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.fontTable+xml"/> '
+    '<Override PartName="/word/theme/theme1.xml" ContentType="application/vnd.openxmlformats-officedocument.theme+xml"/> '
+    '<Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/> '
+    '<Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/> '
+    '<Default Extension="jpeg" ContentType="image/jpeg"/> '
+    '<Default Extension="gif" ContentType="image/gif"/> '
+    '<Default Extension="jpg" ContentType="image/jpeg"/> '
+    '<Default Extension="png" ContentType="image/png"/> '
+    '</Types>')
+    # document.xmlfiles[document.comments] = os.path.join('word', 'comments.xml')
+    return content_types
+
+def setup_comments(document):
+    if document.comments is None:
+        document.comments = etree.fromstring(
+        '<w:comments xmlns:wpc="http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas" '
+        'xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" '
+        'xmlns:o="urn:schemas-microsoft-com:office:office" '
+        'xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" '
+        'xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" '
+        'xmlns:v="urn:schemas-microsoft-com:vml" '
+        'xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" '
+        'xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" '
+        'xmlns:w10="urn:schemas-microsoft-com:office:word" '
+        'xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" '
+        'xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" '
+        'xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" '
+        'xmlns:wpi="http://schemas.microsoft.com/office/word/2010/wordprocessingInk" '
+        'xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml" '
+        'xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" '
+        'mc:Ignorable="w14 wp14"></w:comments>')
+        next_id = '0'
+        document.xmlfiles[document.comments] = os.path.join('word', 'comments.xml')
+    else:
+        next_id = str(len([element for element in document.comments if
+        element.tag == '{' + NSPREFIXES['w'] + '}Comment']))
+    add_content_override(document,  '/word/comments.xml',
+                            'application/vnd.openxmlformats-officedocument'
+                            '.wordprocessingml.comments+xml')
+    helper_functions.add_relationship(document, 'comments.xml', 'http://schemas.'
+    'openxmlformats.org/officeDocument/2006/relationships/comments')
+    return next_id
+
+def add_content_override(document, part_name, content_type):
+    '''checks Types element to see if comments element is included,
+    adds it if not'''
+    flat_contenttypes = sum(
+    [child.items() for child in document.contenttypes.getchildren()], [])
+    if part_name not in [child[1] for child in flat_contenttypes]:
+        document.contenttypes.append(makeelement('Override', nsprefix=None,
+        attributes={'PartName': part_name, 'ContentType': content_type}))
